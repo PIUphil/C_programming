d056916cca33eed568b41fed54b2c4863b43fe7a
PIUphil <ahnkp08@daum.net>
2021-06-17 17:39:08 +0900

    edit except_condition

--- SafeArray/SafeArray2/SafeArray.cpp
+++ SafeArray/SafeArray2/SafeArray.cpp
@@ -14,16 +14,16 @@ SafeArray::SafeArray(const int* pArr, int size): Array(pArr, size) {}
 
 
 int& SafeArray::operator[](int index) {
-	// assert(index >= 0 && index < this->Array::size_);
+	// assert(index < 0 && index >= this->Array::size_);
 	//throw 1;			// 범위를 벗어났을 때. 1을 던짐....(그냥 정해준거임)
-	if (index >= 0 && index < this->Array::size_)
+	if (index < 0 && index >= this->Array::size_)
 		throw InvalidIndex(index);
 	return this->Array::operator[](index);
 }
 const int& SafeArray::operator[](int index) const {
-	//assert(index >= 0 && index < this->Array::size_);
+	//assert(index < 0 && index >= this->Array::size_);
 	//throw 1;
-	if (index >= 0 && index < this->Array::size_)
+	if (index < 0 && index >= this->Array::size_)
 		throw InvalidIndex(index);
 	return this->Array::operator[](index);
 }
