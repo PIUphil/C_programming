d6910c5606e56d62a2055c9dd9043424954b99a9
PIUphil <ahnkp08@daum.net>
2021-06-17 15:41:39 +0900

    edit code (array size -low to high)

--- SafeArray/BoundArray/BoundArray.cpp
+++ SafeArray/BoundArray/BoundArray.cpp
@@ -1,16 +1,39 @@
+#include "InvalidIndex.h"
 #include "BoundArray.h"
 
-BoundArray::BoundArray(int low, int size): SafeArray(size+1),low_(low) {}
+BoundArray::BoundArray(int low, int high) : SafeArray(high-low+1), low_(low) {}
 
+int BoundArray::lower() const {
+	return low_;
+}
+
+int BoundArray::upper() const {
+	return low_ + Array::size_;
+}
+
+int& BoundArray::operator[](int index) {
+	if (index < low_ || index >= upper())
+		throw InvalidIndex(index);
+	return SafeArray::operator[](index - low_);
+}
+
+const int& BoundArray::operator[](int index) const {
+	if (index < low_ || index >= upper())
+		throw InvalidIndex(index);
+	return SafeArray::operator[](index - low_);
+}
+
+/*
+BoundArray::BoundArray(int low, int size): SafeArray(size+1),low_(low) {}
 
 int& BoundArray::operator[](int index) {
 	// throw InvalidIndex(index);
-	return this->Array::operator[](index);
+	return Array::operator[](index);
 }
 
 const int& BoundArray::operator[](int index) const {
 	// throw InvalidIndex(index);
-	return this->Array::operator[](index);
+	return Array::operator[](index);
 }
 
 
@@ -19,5 +42,6 @@ int BoundArray::lower() const {
 }
 
 int BoundArray::upper() const {
-	return this->Array::size_;
+	return Array::size_;
 }
+*/
